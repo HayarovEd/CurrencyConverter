c1d2f60d5f6b1cbe4d6351adfa7afbe977a8cf1f
hayarovy <edurd@rambler.ru>
2022-03-10 08:48:23 +0500

    maked start layout and item of recyclerview

--- app/src/main/java/com/edurda77/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/edurda77/currencyconverter/ui/MainActivity.kt
@@ -3,10 +3,12 @@ package com.edurda77.currencyconverter.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.edurda77.currencyconverter.R
+import com.edurda77.currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
