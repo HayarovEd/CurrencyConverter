780480e4edc35519e9a90df159cd23c50eaed5ae
hayarovy <edurd@rambler.ru>
2022-03-10 08:24:44 +0500

    Initial commit

---
+++ app/src/main/java/com/edurda77/currencyconverter/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.edurda77.currencyconverter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edurda77.currencyconverter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
